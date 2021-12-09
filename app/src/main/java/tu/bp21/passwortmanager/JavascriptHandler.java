5dec8472d834c1f4dfe45f11f7c9aae3d2d541ba
vadim <59964560+vadim-spiro@users.noreply.github.com>
2021-12-09 14:26:21 +0100

    cleaned up files according to google style

--- app/src/main/java/tu/bp21/passwortmanager/JavascriptHandler.java
+++ app/src/main/java/tu/bp21/passwortmanager/JavascriptHandler.java
@@ -4,49 +4,52 @@ import android.webkit.JavascriptInterface;
 import tu.bp21.passwortmanager.DB.PasswortDao;
 import tu.bp21.passwortmanager.DB.User;
 
+/** Framework that can be used in JavaScript. */
 public class JavascriptHandler {
-    private PasswortDao passwortDao;
+  private final PasswortDao passwortDao;
 
-    public JavascriptHandler(PasswortDao dao){
-        passwortDao = dao;
+  public JavascriptHandler(PasswortDao dao) {
+    passwortDao = dao;
+  }
+
+  @JavascriptInterface
+  public boolean existUser(String user) {
+    return passwortDao.getUser(user) != null;
+  }
+
+  @JavascriptInterface
+  public boolean checkUser(String username, String hash) {
+    User user = passwortDao.getUser(username);
+    return user != null && user.password.equals(hash);
+  }
+
+  @JavascriptInterface
+  public boolean createUser(String user, String email, String hash) {
+    User newUser = new User(user, email, hash);
+
+    try {
+      passwortDao.addUser(newUser);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
     }
 
-    //Methoden für Anmeldung
-    @JavascriptInterface
-    public boolean existUser(String user){
-        return passwortDao.getUser(user) != null;
-    }
+    return true;
+  }
 
-    @JavascriptInterface
-    public boolean checkUser(String username, String hash){
-        User user = passwortDao.getUser(username);
-        return user != null && user.password.equals(hash);
-    }
-
-    @JavascriptInterface
-    public boolean createUser(String user, String email, String hash){
-        User newUser = new User(user, email, hash);
-
-        try {
-            passwortDao.addUser(newUser);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            return false;
-        }
-
-        return true;
-    }
-
-    //Methoden für Datenbankabfragen
-    @JavascriptInterface
-    public String getPasswordList(String user){
-        String data = "admin";
-        String output = "{\"length\":," + 1 +
-                "\"websites\":[" + data + "]," +
-                "\"loginnames\":[" + data + "]," +
-                "\"passwords\":[" + data + "]}";
-
-        return output;
-    }
+  @JavascriptInterface
+  public String getPasswordList(String user) {
+    String data = "admin";
+    return "{\"length\":,"
+        + 1
+        + "\"websites\":["
+        + data
+        + "],"
+        + "\"loginnames\":["
+        + data
+        + "],"
+        + "\"passwords\":["
+        + data
+        + "]}";
+  }
 }
--- app/src/main/java/tu/bp21/passwortmanager/MainActivity.java
+++ app/src/main/java/tu/bp21/passwortmanager/MainActivity.java
@@ -1,56 +1,56 @@
 package tu.bp21.passwortmanager;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
-
 import android.annotation.SuppressLint;
 import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 import tu.bp21.passwortmanager.DB.PasswordDatabase;
 
+/** Main entry point for app. */
 public class MainActivity extends AppCompatActivity {
-	private WebView webView;
+  private WebView webView;
 
-	@Override
-	@SuppressLint("SetJavaScriptEnabled")
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+  @Override
+  @SuppressLint("SetJavaScriptEnabled")
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-		try{
-			this.getSupportActionBar().hide();
-		} catch (NullPointerException e) {}
+    if (getSupportActionBar() != null) {
+      getSupportActionBar().hide();
+    }
 
-		//if your build is in debug mode, enable webviews inspection
-		if (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE)) {
-			WebView.setWebContentsDebuggingEnabled(true);
-		}
+    // if your build is in debug mode, enable webviews inspection
+    if (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE)) {
+      WebView.setWebContentsDebuggingEnabled(true);
+    }
 
-		PasswordDatabase database = Room.databaseBuilder(getApplicationContext(),
-				PasswordDatabase.class, "database").allowMainThreadQueries().build();
+    PasswordDatabase database =
+        Room.databaseBuilder(getApplicationContext(), PasswordDatabase.class, "database")
+            .allowMainThreadQueries()
+            .build();
 
-		webView = new WebView(getApplicationContext());
-		webView.setWebViewClient(new WebViewClient());
-		webView.addJavascriptInterface(new JavascriptHandler(database.getDao()),"Java");
-		webView.loadUrl("file:///android_asset/index.html");
+    webView = new WebView(getApplicationContext());
+    webView.setWebViewClient(new WebViewClient());
+    webView.addJavascriptInterface(new JavascriptHandler(database.getDao()), "Java");
+    webView.loadUrl("file:///android_asset/index.html");
 
-		WebSettings webSettings = webView.getSettings();
-		webSettings.setJavaScriptEnabled(true);
-		webSettings.setAllowFileAccess(true);
-		
+    WebSettings webSettings = webView.getSettings();
+    webSettings.setJavaScriptEnabled(true);
+    webSettings.setAllowFileAccess(true);
 
-		setContentView(webView);
-	}
+    setContentView(webView);
+  }
 
-	@Override
-	public void onBackPressed() {
-		if(webView.canGoBack()){
-			webView.goBack();
-		} else {
-			super.onBackPressed();
-		}
-	}
+  @Override
+  public void onBackPressed() {
+    if (webView.canGoBack()) {
+      webView.goBack();
+    } else {
+      super.onBackPressed();
+    }
+  }
 }
