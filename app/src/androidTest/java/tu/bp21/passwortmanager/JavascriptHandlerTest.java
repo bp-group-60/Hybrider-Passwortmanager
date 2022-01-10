62e2f74440539c38e841ed0a3630d710abd1d0f9
T3Cabon <t3cabon@googlemail.com>
2022-01-10 11:23:36 +0100

    jUnit Testfile erstellt

---
+++ app/src/androidTest/java/tu/bp21/passwortmanager/JavascriptHandlerTest.java
@@ -0,0 +1,38 @@
+package tu.bp21.passwortmanager;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JavascriptHandlerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void existUser() {
+    }
+
+    @Test
+    public void checkUser() {
+    }
+
+    @Test
+    public void createUser() {
+    }
+
+    @Test
+    public void createPassword() {
+    }
+
+    @Test
+    public void getPasswordList() {
+    }
+}
---
+++ app/src/androidTest/java/tu/bp21/passwortmanager/WebviewTest.java
@@ -0,0 +1,26 @@
+package tu.bp21.passwortmanager;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WebviewTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onBackPressed() {
+    }
+}
