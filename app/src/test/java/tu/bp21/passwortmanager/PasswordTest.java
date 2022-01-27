7dc3d18526c9d217ad09d7e989a5f62d598bcc14
giangkute7v <giangkute7v@gmail.com>
2022-01-27 20:42:48 +0100

    Klasse PasswordTest angepasst, Klasse UserTest gelöscht da keine Methoden mehr vorhanden sind.

--- app/src/test/java/tu/bp21/passwortmanager/PasswordTest.java
+++ app/src/test/java/tu/bp21/passwortmanager/PasswordTest.java
@@ -6,42 +6,40 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.security.SecureRandom;
+
 import tu.bp21.passwortmanager.db.Password;
 
 public class PasswordTest {
 
     private Password passwordObj;
     private Password wrongPasswordObj;
-    private String user, website, loginName,password;
+    private String user, website, loginName;
+    private byte[] password;
 
     @Before
     public void setUp() throws Exception {
         user = "PasswordTestUser01";
         website = "PasswordTestWebsite01";
         loginName = "PasswordTestLoginName01";
-        password =  "passwordTestpwd01";
+        password =  new byte[20];
+        SecureRandom.getInstanceStrong().nextBytes(password);
 
         passwordObj = new Password(user,website,loginName,password);
-        wrongPasswordObj = new Password(user,"wrong",loginName,"password");
+        wrongPasswordObj = new Password(user,"wrong",loginName,password);
     }
 
     @After
     public void tearDown() throws Exception {
-        user = website = loginName = password = "";
+        user = website = loginName = "";
+        password = null;
         passwordObj = wrongPasswordObj = null;
     }
 
-    @Test
-    public void toSecureString() {
-        String expect = "[\"" + website + "\",\"" + loginName + "\"]";
-        assertEquals(expect, passwordObj.toSecureString());
-        assertNotEquals(expect,wrongPasswordObj.toSecureString());
-
-    }
 
     @Test
     public void testToString() {
-        String expect = "[\"" + website + "\",\"" + loginName + "\",\"" + password + "\"]";
+        String expect = "[\"" + website + "\",\"" + loginName + "\"]";
         assertEquals(expect, passwordObj.toString());
         assertNotEquals(expect,wrongPasswordObj.toString());
     }
