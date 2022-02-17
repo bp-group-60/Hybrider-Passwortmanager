c7b0407e7e6baf736d93d07e5b1adfdf19160846
giangkute7v <giangkute7v@gmail.com>
2022-02-17 22:10:37 +0100

    tests für InterfaceTools

---
+++ app/src/androidTest/java/tu/bp21/passwortmanager/js_interfaces/InterfaceToolsTest.java
@@ -0,0 +1,82 @@
+package tu.bp21.passwortmanager.js_interfaces;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static tu.bp21.passwortmanager.StringFunction.*;
+import static org.junit.jupiter.api.Assumptions.*;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import de.mannodermaus.junit5.ActivityScenarioExtension;
+import tu.bp21.passwortmanager.MainActivity;
+
+class InterfaceToolsTest {
+    static MainActivity mainActivity;
+    ClipboardManager clipboard;
+
+    InterfaceTools interfaceTools;
+
+    @RegisterExtension
+    final ActivityScenarioExtension<MainActivity> scenarioExtension =
+            ActivityScenarioExtension.launch(MainActivity.class);
+
+    @BeforeEach
+    void setUp() {
+        if (mainActivity == null) {
+          ActivityScenario<MainActivity> scenario = scenarioExtension.getScenario();
+          scenario.onActivity(activity -> mainActivity = activity);
+        }
+        clipboard = (ClipboardManager) mainActivity.getSystemService(Context.CLIPBOARD_SERVICE);
+        interfaceTools = new InterfaceTools(mainActivity);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Nested
+    @DisplayName("Tests for copyToClipboardWithTimeout")
+    class copyToClipboardWithTimeoutTest {
+
+        @ParameterizedTest
+        @CsvFileSource(resources = "/Tools/copyToClipboardWithTimeoutTest.csv", numLinesToSkip = 1)
+        @DisplayName("Case: ")
+        void copyToClipboardWithTimeoutSuccess(String displayCase, String text, long timeout) throws Exception{
+            String expectedText = convertNullToEmptyString(text);
+            interfaceTools.copyToClipboardWithTimeout(expectedText, timeout);
+            String actualText = clipboard.getPrimaryClip().getItemAt(0).getText().toString();
+            assertEquals(expectedText, actualText);
+            Thread.sleep(10);
+            assumingThat(timeout>=0, () -> {
+                Thread.sleep(timeout);
+                String currentActualText = clipboard.getPrimaryClip().getItemAt(0).getText().toString();
+                assertEquals("",currentActualText);
+                assertEquals(1, clipboard.getPrimaryClip().getItemCount());
+            });
+        }
+    }
+
+    @Test
+    void clearClipboardTest() throws Exception{
+        String textToCopy = generateRandomString(100);
+        String lable = generateRandomString(100);
+        clipboard.setPrimaryClip(ClipData.newPlainText(lable, textToCopy));
+        interfaceTools.clearClipboard();
+        Thread.sleep(10);
+        String actualText = clipboard.getPrimaryClip().getItemAt(0).getText().toString();
+        assertEquals("",actualText);
+        assertEquals(1, clipboard.getPrimaryClip().getItemCount());
+    }
+}
