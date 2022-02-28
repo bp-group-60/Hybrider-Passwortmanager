36d74a72e6c17adb423afdca3a100524ef5a07c8
T3Cabon <t3cabon@googlemail.com>
2022-02-28 16:03:42 +0100

    Makefile - Pfad korrigiert passwordGenerate.c - neue Methoden für Datenfluss integriert - TODO!

--- app/src/main/assets/src/webAssembly/passwordGenerate.c
+++ app/src/main/assets/src/webAssembly/passwordGenerate.c
@@ -4,36 +4,70 @@
 #include <stdio.h>
 #include <emscripten.h> // note we added the emscripten header
 
-void EMSCRIPTEN_KEEPALIVE GenerateRandomPasswortbyC06( int * password, int password_size){
-        int randomizer = 0;
+void EMSCRIPTEN_KEEPALIVE GenerateRandomUsername(int *username, int username_size) {
+    int randomizer = 0;
 
-        // Seed the random-number generator
-        // with current time so that the
-        // numbers will be different every time
-        srand((unsigned int)(time(NULL)));
+    // Seed the random-number generator
+    // with current time so that the
+    // numbers will be different every time
+    srand((unsigned int) (time(NULL)));
 
-        char allNumbers[] = "0123456789";
-        char allLowerletter[] = "abcdefghijklmnopqrstuvwxyz";
-        char allCapsLetter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        char allSymbols[] = "!@#$^&*?";
+    char allNumbers[] = "0123456789";
+    char allLowerletter[] = "abcdefghijklmnopqrstuvwxyz";
+    char allCapsLetter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char allSymbols[] = "!@#$^&*?";
 
-        int i = 0;
+    int i = 0;
 
-        for (i = 0; i < password_size ; i++) {
-            
-            randomizer = rand() % 4;
+    for (i = 0; i < username_size; i++) {
 
-            if (randomizer == 1) {
-                password[i] = (int)(allNumbers[rand() % (sizeof(allNumbers)-1)]);
-            }
-            else if (randomizer == 2) {
-                password[i] = (int)(allSymbols[rand() %  (sizeof(allSymbols)-1)]);
-            }
-            else if (randomizer == 3) {
-                password[i] = (int)(allCapsLetter[rand() %  (sizeof(allCapsLetter)-1)]);
-            }
-            else {
-                password[i] = (int)(allLowerletter[rand() %  (sizeof(allLowerletter)-1)]);
-            }
+        randomizer = rand() % 2;
+
+        if (randomizer == 1) {
+            username[i] = (int) (allNumbers[rand() % (sizeof(allNumbers) - 1)]);
+        } else if (randomizer == 2) {
+            username[i] = (int) (allCapsLetter[rand() % (sizeof(allCapsLetter) - 1)]);
+        } else {
+            username[i] = (int) (allLowerletter[rand() % (sizeof(allLowerletter) - 1)]);
         }
     }
+}
+
+void EMSCRIPTEN_KEEPALIVE GenerateRandomPasswort(int *password, int password_size) {
+    int randomizer = 0;
+
+    // Seed the random-number generator
+    // with current time so that the
+    // numbers will be different every time
+    srand((unsigned int) (time(NULL)));
+
+    char allNumbers[] = "0123456789";
+    char allLowerletter[] = "abcdefghijklmnopqrstuvwxyz";
+    char allCapsLetter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char allSymbols[] = "!@#$^&*?";
+
+    int i = 0;
+
+    for (i = 0; i < password_size; i++) {
+
+        randomizer = rand() % 4;
+
+        if (randomizer == 1) {
+            password[i] = (int) (allNumbers[rand() % (sizeof(allNumbers) - 1)]);
+        } else if (randomizer == 2) {
+            password[i] = (int) (allSymbols[rand() % (sizeof(allSymbols) - 1)]);
+        } else if (randomizer == 3) {
+            password[i] = (int) (allCapsLetter[rand() % (sizeof(allCapsLetter) - 1)]);
+        } else {
+            password[i] = (int) (allLowerletter[rand() % (sizeof(allLowerletter) - 1)]);
+        }
+    }
+}
+
+void EMSCRIPTEN_KEEPALIVE getReverseUsername(int *username) {
+
+}
+
+void EMSCRIPTEN_KEEPALIVE getReversePassword(int *password) {
+
+}
