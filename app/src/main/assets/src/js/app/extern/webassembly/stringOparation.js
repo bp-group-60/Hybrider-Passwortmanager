9bf9e4ea3bdb02713e3c8a850ac1e6c5089a7b98
Imagee <47892322+Imagee@users.noreply.github.com>
2022-03-17 17:06:28 +0100

    stringOperations.js

---
+++ app/src/main/assets/src/js/app/extern/webassembly/stringOparation.js
@@ -0,0 +1,42 @@
+
+
+export function generateRandomString(length){
+    let RandomStringModule = Module.cwrap("GenerateRandomString", null, ["number","number"]);
+    let byteOffset = 0;
+    let outputArray = new Int32Array(Module.HEAP32.buffer, byteOffset, length);
+    let bytesPerElement = outputArray.BYTES_PER_ELEMENT;
+
+    let outputPointer = Module._malloc(length * bytesPerElement);
+
+    Module.HEAP32.set(outputArray, (outputPointer / bytesPerElement));
+    RandomStringModule(outputPointer, length);
+
+    outputArray = new Int32Array(Module.HEAP32.buffer, outputPointer, length);
+    let generatedPassword = String.fromCharCode.apply(null, outputArray);
+
+    Module._free(outputPointer);
+
+    return generatedPassword
+}
+
+export function IdentityFunction(inputString){
+    let IdentityStringFunction = Module.cwrap("IdentityFunction", null, ["number","number","number"]);
+    let length = inputString.length;
+    let stringAsCharCodeArray = Array.from([...inputString], (x) => x.charCodeAt(0));
+    let inputArray = Int32Array.from(stringAsCharCodeArray);
+    let bytesPerElement = inputArray.BYTES_PER_ELEMENT;
+
+    let inputPointer = Module._malloc(length * bytesPerElement);
+    let outputPointer = Module._malloc(length * bytesPerElement);
+
+    Module.HEAP32.set(inputArray, (inputPointer / bytesPerElement));
+    IdentityStringFunction(inputPointer,outputPointer, length);
+
+    let outputArray = new Int32Array(Module.HEAP32.buffer, outputPointer, length);
+    let generatedString = String.fromCharCode.apply(null, outputArray);
+
+    Module._free(inputPointer);
+    Module._free(outputPointer);
+
+    return generatedString;
+}
