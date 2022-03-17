8bf993c089e9f7333a3d6a90f16e88ee7f2e86f0
giangkute7v <giangkute7v@gmail.com>
2022-03-17 23:15:41 +0100

    UITest

---
+++ app/src/androidTest/java/tu/bp21/passwortmanager/ui/AddPasswordTest.java
@@ -0,0 +1,99 @@
+package tu.bp21.passwortmanager.ui;
+
+import androidx.room.Room;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.web.webdriver.Locator;
+import static androidx.test.espresso.web.sugar.Web.onWebView;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.webClick;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.webKeys;
+import static androidx.test.espresso.web.assertion.WebViewAssertions.*;
+import static androidx.test.espresso.web.matcher.DomMatchers.containingTextInBody;
+
+import org.junit.jupiter.api.AfterAll;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import de.mannodermaus.junit5.ActivityScenarioExtension;
+import tu.bp21.passwortmanager.MainActivity;
+import tu.bp21.passwortmanager.db.database.ApplicationDatabase;
+import static tu.bp21.passwortmanager.StringFunction.*;
+
+public class AddPasswordTest {
+    @RegisterExtension
+    final ActivityScenarioExtension<MainActivity> scenarioExtension =
+            ActivityScenarioExtension.launch(MainActivity.class);
+
+    static MainActivity mainActivity;
+    static ApplicationDatabase database;
+    static String randomUsername;
+    static String randomEmail;
+    static String randomMasterPassword;
+    String randomWebsiteName;
+    String randomLoginName;
+    String randomLoginPassword;
+    static final int stringLength = 20;
+    static final int smallStringLength = 5;
+    static final int masterPasswordMinLength = 8;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        if (mainActivity == null) {
+            ActivityScenario<MainActivity> scenario = scenarioExtension.getScenario();
+            scenario.onActivity(activity -> mainActivity = activity);
+
+            database =
+                    Room.databaseBuilder(mainActivity, ApplicationDatabase.class, "testDatabase")
+                            .allowMainThreadQueries()
+                            .build();
+
+            randomUsername = generateRandomString(stringLength);
+            randomEmail = generateRandomString(stringLength) + "@" + generateRandomString(smallStringLength) + "." + generateRandomString(smallStringLength);
+            do {
+                randomMasterPassword = generateRandomString(stringLength);
+            }
+            while(randomMasterPassword.length() < masterPasswordMinLength);
+
+            onWebView().forceJavascriptEnabled();
+
+            //create user for the test
+            onWebView().withElement(findElement(Locator.LINK_TEXT, "Konto erstellen")).perform(webClick());
+            onWebView().withElement(findElement(Locator.ID, "signup-username")).perform(webKeys(randomUsername));
+            onWebView().withElement(findElement(Locator.ID, "signup-email")).perform(webKeys(randomEmail));
+            onWebView().withElement(findElement(Locator.ID, "signup-password")).perform(webKeys(randomMasterPassword));
+            onWebView().withElement(findElement(Locator.ID, "signup-password-confirm")).perform(webKeys(randomMasterPassword));
+            onWebView().withElement(findElement(Locator.ID, "submit-button")).perform(webClick());
+            onWebView().check(webContent(containingTextInBody("Anmeldung")));
+            onWebView().check(webContent(containingTextInBody("Benutzerkonto erfolgreich erstellt.")));
+        }
+        //login
+        onWebView().withElement(findElement(Locator.ID, "input-username")).perform(webKeys(randomUsername));
+        onWebView().withElement(findElement(Locator.ID, "input-userPassword")).perform(webKeys(randomMasterPassword));
+        onWebView().withElement(findElement(Locator.ID, "login")).perform(webClick());
+        onWebView().check(webContent(containingTextInBody("Webseite / Nutzername")));
+
+        randomWebsiteName = generateRandomString(stringLength);
+        randomLoginName = generateRandomString(stringLength);
+        randomLoginPassword = generateRandomString(stringLength);
+    }
+
+    @Test
+    void addPasswordSuccess() throws Exception{
+        onWebView().withElement(findElement(Locator.ID, "add-button")).perform(webClick());
+        //onWebView().check(webContent(containingTextInBody("Neues Passwort")));
+        Thread.sleep(3000);
+        onWebView().withElement(findElement(Locator.CSS_SELECTOR, "input[placeholder=Webseite]")).perform(webKeys(randomWebsiteName));
+        onWebView().withElement(findElement(Locator.CSS_SELECTOR, "input[placeholder=Loginname]")).perform(webKeys(randomLoginName));
+        onWebView().withElement(findElement(Locator.CSS_SELECTOR, "input[placeholder=Passwort]")).perform(webKeys(randomLoginPassword));
+        onWebView().withElement(findElement(Locator.ID, "commit-button")).perform(webClick());
+        onWebView().check(webContent(containingTextInBody(randomLoginName)));
+
+    }
+
+    @AfterAll
+    static void tearDown() throws Exception {
+        mainActivity.deleteDatabase("testDatabase");
+    }
+}
---
+++ app/src/androidTest/java/tu/bp21/passwortmanager/ui/LoginTest.java
@@ -0,0 +1,98 @@
+package tu.bp21.passwortmanager.ui;
+
+import androidx.room.Room;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.web.webdriver.Locator;
+import static androidx.test.espresso.web.sugar.Web.onWebView;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.webClick;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.webKeys;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.getText;
+import static androidx.test.espresso.web.assertion.WebViewAssertions.*;
+import static androidx.test.espresso.web.matcher.DomMatchers.hasElementWithId;
+import static androidx.test.espresso.web.matcher.DomMatchers.containingTextInBody;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+import androidx.test.filters.LargeTest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import de.mannodermaus.junit5.ActivityScenarioExtension;
+import tu.bp21.passwortmanager.MainActivity;
+import tu.bp21.passwortmanager.db.database.ApplicationDatabase;
+import static tu.bp21.passwortmanager.StringFunction.*;
+
+public class LoginTest {
+    @RegisterExtension
+    final ActivityScenarioExtension<MainActivity> scenarioExtension =
+            ActivityScenarioExtension.launch(MainActivity.class);
+
+    static MainActivity mainActivity;
+    static ApplicationDatabase database;
+    static String randomUsername;
+    static String randomEmail;
+    static String randomPassword;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        if (mainActivity == null) {
+            ActivityScenario<MainActivity> scenario = scenarioExtension.getScenario();
+            scenario.onActivity(activity -> mainActivity = activity);
+
+            database =
+                    Room.databaseBuilder(mainActivity, ApplicationDatabase.class, "testDatabase")
+                            .allowMainThreadQueries()
+                            .build();
+
+            randomUsername = generateRandomString(20);
+            randomEmail = generateRandomString(20) + "@" + generateRandomString(5) + "." + generateRandomString(5);
+            do {
+                randomPassword = generateRandomString(20);
+            }
+            while(randomPassword.length() <8);
+
+            onWebView().forceJavascriptEnabled();
+            onWebView().withElement(findElement(Locator.LINK_TEXT, "Konto erstellen")).perform(webClick());
+            onWebView().withElement(findElement(Locator.ID, "signup-username")).perform(webKeys(randomUsername));
+            onWebView().withElement(findElement(Locator.ID, "signup-email")).perform(webKeys(randomEmail));
+            onWebView().withElement(findElement(Locator.ID, "signup-password")).perform(webKeys(randomPassword));
+            onWebView().withElement(findElement(Locator.ID, "signup-password-confirm")).perform(webKeys(randomPassword));
+            onWebView().withElement(findElement(Locator.ID, "submit-button")).perform(webClick());
+            onWebView().check(webContent(containingTextInBody("Anmeldung")));
+            onWebView().check(webContent(containingTextInBody("Benutzerkonto erfolgreich erstellt.")));
+        }
+    }
+
+    @Test
+    void loginSuccessTest() throws Exception{
+        onWebView().withElement(findElement(Locator.ID, "input-username")).perform(webKeys(randomUsername));
+        onWebView().withElement(findElement(Locator.ID, "input-userPassword")).perform(webKeys(randomPassword));
+        onWebView().withElement(findElement(Locator.ID, "login")).perform(webClick());
+        onWebView().check(webContent(containingTextInBody("Webseite / Nutzername")));
+    }
+
+    @Test
+    void loginFailureTest(){
+        String differentPassword;
+        do{
+            differentPassword = generateRandomString(20);
+        } while (differentPassword == randomPassword);
+        onWebView().withElement(findElement(Locator.ID, "input-username")).perform(webKeys(randomUsername));
+        onWebView().withElement(findElement(Locator.ID, "input-userPassword")).perform(webKeys(differentPassword));
+        onWebView().withElement(findElement(Locator.ID, "login")).perform(webClick());
+        onWebView().check(webContent(containingTextInBody("Anmeldung")));
+        onWebView().check(webContent(containingTextInBody("Benutzername oder falsches Passwort")));
+    }
+
+    @AfterAll
+    static void tearDown() throws Exception {
+        mainActivity.deleteDatabase("testDatabase");
+    }
+}
+
+
