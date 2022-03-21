64804893a655a0909bd3667f3c390182f231effa
giangkute7v <giangkute7v@gmail.com>
2022-03-21 13:39:49 +0100

    parameters ändern

--- app/src/main/java/tu/bp21/passwortmanager/Constants.java
+++ app/src/main/java/tu/bp21/passwortmanager/Constants.java
@@ -15,11 +15,11 @@ public class Constants {
     //parameters for scrypt
     //Parameter N as the n power of 2 for scrypt parameter N, for example 2^n
     //total ram cost = 128 * 2^n * r * p bytes
-    public static final int SCRYPT_PARAM_N = 15;
-    public static final int SCRYPT_PARAM_R = 5;
+    public static final int SCRYPT_PARAM_N = 18;
+    public static final int SCRYPT_PARAM_R = 8;
     public static final int SCRYPT_PARAM_P = 1;
     //length of hash value of user password stored in database
-    public static final int HASH_OUTPUT_LENGTH = 60;
+    public static final int HASH_OUTPUT_LENGTH = 64;
 
     //parameters for PBKDF2
     public static final int PBKDF2_ITERATIONS = 500000;
